097459e9239ff2d5d0468b86e7b51143edd7ab49
Hussein Ben Farkhani <hussein.ben.dev@gmail.com>
2018-01-29 15:09:49 +0000

    update again please ignore

--- iLost tracker/raspberry pi python scripts/long-range/start_server.sh
+++ iLost tracker/raspberry pi python scripts/long-range/start_server.sh
@@ -5,6 +5,22 @@
 while true;
 
 	sudo hologram recieve >> log.txt
-	sleep 60s;
+	sleep 5s;
 
 done 
+
+
+
+
+PATTERN=$1
+FILE=$2
+
+if grep -q $PATTERN $FILE;
+ then
+     echo "Here are the Strings with the Pattern '$PATTERN':"
+     echo -e "$(grep $PATTERN $FILE)\n"
+ else
+     echo "Error: The Pattern '$PATTERN' was NOT Found in '$FILE'"
+     echo "Exiting..."
+     exit 0
+fi
