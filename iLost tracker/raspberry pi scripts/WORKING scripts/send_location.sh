df159c2012df322a56bdeaeb24e28682673f9ca1
Hussein Ben Farkhani <hussein.ben.dev@gmail.com>
2018-01-30 23:34:34 +0000

    updated-script
    this script is automatic, will send location back to user automatically

---
+++ iLost tracker/raspberry pi scripts/WORKING scripts/send_location.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+while :
+
+do
+
+if grep rl typescript
+then
+	echo "yup , I got the location request"
+	location=$(sudo hologram modem location) && sudo hologram send "$location" -t "LR"
+	>typescript
+	echo "sent location back to user"
+else
+	echo "..."
+	sleep 10s
+fi
+
+done
